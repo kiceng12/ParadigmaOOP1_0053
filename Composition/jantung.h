714ebebb600da8358d8a99db7586cc92f844bd7c
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-20 18:53:12 +0700

    class jantung

---
+++ Composition/jantung.h
@@ -0,0 +1,15 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+    public:
+        jantung(){
+            cout << "jantung dihidupkan\"";
+        }
+        ~jantung(){
+            cout << "jantung dimatikan\"";
+        }
+};
+#endif
