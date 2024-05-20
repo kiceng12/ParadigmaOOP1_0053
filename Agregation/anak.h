ddf48edb51a6399bce782607df9a1d1aad329770
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-20 18:32:48 +0700

    public

--- Agregation/anak.h
+++ Agregation/anak.h
@@ -2,6 +2,15 @@
 #define ANAK_H
 
 class anak {
+    public:
+    string name;
+    jantung varJantung;
 
-
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
 };
