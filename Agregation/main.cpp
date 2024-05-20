21880ac6b0bd579414bc60da207fac561a281461
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-20 18:44:22 +0700

    main.cpp

---
+++ Agregation/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main() {
+    ibu* varIbu = new ibu("disini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu->tambahAnak(varAnak3);
+    varIbu->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+}
